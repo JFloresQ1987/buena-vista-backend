142be0c2f021289797cebff654a521e6bca41767
jhon <jhon.ichaccaya.27@unsch.edu.pe>
2020-09-03 18:57:00 -0500

    funcionalidad listar usuario, actualizar usuario terminada

--- controllers/core/seguridad/usuarios.controller.js
+++ controllers/core/seguridad/usuarios.controller.js
@@ -1,56 +1,160 @@
 const { response } = require("express");
 const bcrypt = require("bcryptjs");
+const dayjs = require("dayjs");
 const Usuario = require("../../../models/core/seguridad/usuario.model");
 const Persona = require("../../../models/core/registro/persona.model");
 
 const listar = async (req, res) => {
-    //const modelo = await Usuario.find({ "es_borrado": false }, 'usuario debe_cambiar_clave_inicio_sesion es_bloqueado es_vigente')
-    const usuarios = await Usuario.find(
-        { es_borrado: false },
-        "usuario debe_cambiar_clave_inicio_sesion es_bloqueado es_vigente"
-    ).populate('persona','nombre apellido_paterno apellido_materno');
+  //const modelo = await Usuario.find({ "es_borrado": false }, 'usuario debe_cambiar_clave_inicio_sesion es_bloqueado es_vigente')
+  const usuarios = await Usuario.find(
+    { es_borrado: false },
+    "usuario debe_cambiar_clave_inicio_sesion es_bloqueado es_vigente"
+  )
+    .populate("persona", "nombre apellido_paterno apellido_materno")
+    .sort({ persona: -1 });
+  //).populate('persona','nombre apellido_paterno apellido_materno');
 
-    const total = await Usuario.find({ es_borrado: false }).count();
+  const total = await Usuario.find({ es_borrado: false }).countDocuments();
 
-    res.json({
-        ok: true,
-        usuarios,
-        total,
-    });
+  res.json({
+    ok: true,
+    usuarios,
+    total,
+  });
 };
 
 const crear = async (req, res = response) => {
-    const { usuario, clave } = req.body;
+  //const { usuario, clave } = req.body;
 
-    // console.log('entroo 1')
+  try {
+    const { documento_identidad, comentario } = req.body;
+    const existe_registro = await Persona.findOne({ documento_identidad });
 
-    try {
-        const existe_usuario = await Usuario.findOne({ usuario });
-
-        if (existe_usuario)
-            return res.status(400).json({
-                ok: false,
-                msg: "El usuario ya esta registrado.",
-            });
-
-        const modelo = new Usuario(req.body);
-
-        const salt = bcrypt.genSaltSync();
-        modelo.clave = bcrypt.hashSync(clave, salt);
-
-        await modelo.save();
-
-        res.json({
-            ok: true,
-            modelo,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: "Error inesperado.",
-        });
+    if (existe_registro) {
+      return res.status(400).json({
+        ok: false,
+        msg: "La persona ya esta registrado.",
+      });
     }
+
+    const persona = new Persona(req.body);
+    const now = dayjs();
+    persona.comentario = [
+      {
+        tipo: "Nuevo",
+        usuario: req.header("usuario"),
+        nombre: req.header("nombre"),
+        fecha: now.format("DD/MM/YYYY hh:mm:ss a"),
+        comentario,
+      },
+    ];
+    const existe_usuario = await Usuario.findOne({ documento_identidad });
+    if (existe_usuario)
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario ya esta registrado.",
+      });
+
+    persona.save();
+
+    usuario = {
+      usuario: persona.documento_identidad,
+      clave: persona.documento_identidad,
+      persona: persona._id,
+      rol: req.body.rol,
+    };
+
+    const modelo = new Usuario(usuario);
+
+    const salt = bcrypt.genSaltSync();
+    modelo.clave = bcrypt.hashSync(usuario.clave, salt);
+
+    await modelo.save();
+
+    res.json({
+      ok: true,
+      modelo,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado.",
+    });
+  }
+};
+
+const getUsuario = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const usuario = await Usuario.findById(id).populate(
+      "persona",
+      "nombre apellido_paterno apellido_materno fecha_nacimiento documento_identidad es_masculino numero_telefono numero_celular correo_electronico domicilio referencia_domicilio comentario"
+    );
+    if (usuario) {
+      res.json({
+        ok: true,
+        usuario,
+      });
+    } else {
+      res.json({
+        ok: false,
+        msg: "Usuario no encontrado",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: error,
+    });
+  }
+};
+
+const actualizar = async (req, res = response) => {
+  try {
+    const id = req.params.id;
+    const { rol, comentario, documento_identidad } = req.body;
+    const now = dayjs();
+    const usuarioM = await Usuario.findById(id);
+    if (!usuarioM) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe",
+      });
+    }
+    usuarioM.rol = rol;
+    await usuarioM.save();
+
+    const persona = await Persona.findOne({ documento_identidad });
+    persona.nombre = req.body.nombre;
+
+    persona.apellido_paterno = req.body.apellido_paterno;
+    persona.apellido_materno = req.body.apellido_materno;
+    persona.fecha_nacimiento = req.body.fecha_nacimiento;
+
+    persona.numero_celular = req.body.numero_celular;
+    persona.numero_telefono = req.body.numero_telefono;
+    persona.correo_electronico = req.body.correo_electronico;
+    persona.domicilio = req.body.domicilio;
+
+    persona.referencia_domicilio = req.body.referencia_domicilio;
+    persona.comentario.push({
+      tipo: "Editar",
+      usuario: req.header("usuario"),
+      nombre: req.header("nombre"),
+      fecha: now.format("DD/MM/YYYY hh:mm:ss a"),
+      comentario,
+    });
+    await persona.save();
+    res.json({
+      ok: true,
+      msg: "Usuario editado Correctamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado.",
+    });
+  }
 };
 
 // const actualizar = async (req, res = response) => {
@@ -109,6 +213,8 @@ const crear = async (req, res = response) => {
 // }
 
 module.exports = {
-    listar,
-    crear,
+  listar,
+  crear,
+  getUsuario,
+  actualizar,
 };
--- routes/core/seguridad/usuarios.route.js
+++ routes/core/seguridad/usuarios.route.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../../../middlewares/validar-campos');
 const { validarJWT, validarAuthorization } = require('../../../middlewares/validar-jwt');
-const { listar, crear } = require('../../../controllers/core/seguridad/usuarios.controller');
+const { listar, crear, getUsuario, actualizar } = require('../../../controllers/core/seguridad/usuarios.controller');
 
 const { fake } = require('../../../controllers/test');
 
@@ -11,15 +11,37 @@ const router = Router();
 router.post('/fake', fake);
 
 router.get('/', validarJWT, listar);
+router.get('/:id', validarJWT, getUsuario);
 
 router.post('/', [
     validarJWT,
     validarAuthorization,
-    check('usuario', 'El usuario es obligatorio').notEmpty(),
-    check('clave', 'La clave es obligatorio').notEmpty(),
-    check('debe_cambiar_clave_inicio_sesion', 'Debe cambiar clave es obligatorio').notEmpty(),
+    check('documento_identidad', 'El documento de identidad es obligatorio').notEmpty(),
+    check('nombre', 'El nombre es obligatorio').notEmpty(),
+    check('apellido_paterno', 'El apellido paterno es obligatorio').notEmpty(),
+    check('apellido_materno', 'El apellido materno es obligatorio').notEmpty(),
+    check('fecha_nacimiento', 'La fecha de nacimiento es obligatorio').notEmpty(),
+    check('es_masculino', 'La fecha de nacimiento es obligatorio').notEmpty(),
+    check('domicilio', 'El domicilio es obligatorio').notEmpty(),
+    check('referencia_domicilio', 'La referencia del domicilio es obligatorio').notEmpty(),
+    check('comentario', 'El comentario es obligatorio').notEmpty(),
     validarCampos
 ], crear);
+
+router.put('/:id', [
+    validarJWT,
+    //validarAuthorization,
+    check('documento_identidad', 'El documento de identidad es obligatorio').notEmpty(),
+    check('nombre', 'El nombre es obligatorio').notEmpty(),
+    check('apellido_paterno', 'El apellido paterno es obligatorio').notEmpty(),
+    check('apellido_materno', 'El apellido materno es obligatorio').notEmpty(),
+    check('fecha_nacimiento', 'La fecha de nacimiento es obligatorio').notEmpty(),
+    check('es_masculino', 'La fecha de nacimiento es obligatorio').notEmpty(),
+    check('domicilio', 'El domicilio es obligatorio').notEmpty(),
+    check('referencia_domicilio', 'La referencia del domicilio es obligatorio').notEmpty(),
+    check('comentario', 'El comentario es obligatorio').notEmpty(),
+    validarCampos
+], actualizar);
 
 // router.put('/:id',
 //     [
