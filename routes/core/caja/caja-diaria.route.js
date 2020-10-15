df09ec8ba8c04691831fd8864e71738695db9d20
anth97 <andy.luya@unsch.edu.pe>
2020-10-14 20:05:14 -0500

    listar caja

--- routes/core/caja/caja-diaria.route.js
+++ routes/core/caja/caja-diaria.route.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../../../middlewares/validar-campos');
 const { validarJWT } = require('../../../middlewares/validar-jwt');
-const { cargarCaja, cerrarCaja, listarCajas } = require('../../../controllers/core/caja/caja-diario.controller');
+const { cargarCaja, cerrarCaja, listarCajas, listarCajasPorFecha } = require('../../../controllers/core/caja/caja-diario.controller');
 
 const router = Router();
 
@@ -11,6 +11,8 @@ router.put('/:id', validarJWT, cerrarCaja);
 router.get('/caja', validarJWT, cargarCaja);
 
 router.get('/', validarJWT, listarCajas);
+
+router.get('/fecha/:fecha_apertura', validarJWT, listarCajasPorFecha);
 
 
 
