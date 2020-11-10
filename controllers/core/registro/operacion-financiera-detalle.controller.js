8b69749cf96f007c48f9e9d83991295ac928ec58
jhon <jhon.ichaccaya.27@unsch.edu.pe>
2020-11-10 12:32:10 -0500

    reporte y actualizar cuotas

--- controllers/core/registro/operacion-financiera-detalle.controller.js
+++ controllers/core/registro/operacion-financiera-detalle.controller.js
@@ -1,33 +1,92 @@
-const { response } = require('express');
-const logger = require('../../../helpers/logger');
-const OperacionFinancieraDetalle = require('../../../models/core/registro/operacion-financiera-detalle.model');
+const { response } = require("express");
+const logger = require("../../../helpers/logger");
+const OperacionFinancieraDetalle = require("../../../models/core/registro/operacion-financiera-detalle.model");
 // const dayjs = require('dayjs');
 
-const listar_operaciones_financieras_detalle = async(req, res) => {
+const listar_operaciones_financieras_detalle = async (req, res) => {
+  // const { id_operacion_financiera } = req.body;
+  const id_operacion_financiera = req.params.id_operacion_financiera;
 
-    // const { id_operacion_financiera } = req.body;
-    const id_operacion_financiera = req.params.id_operacion_financiera;
+  try {
+    const lista = await OperacionFinancieraDetalle.find({
+      operacion_financiera: id_operacion_financiera,
+      es_borrado: false,
+      es_vigente: true,
+    }).sort({ numero_cuota: 1 });
 
-    try {
+    res.json({
+      ok: true,
+      lista,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado.",
+    });
+  }
+};
 
-        const lista = await OperacionFinancieraDetalle.find({ "operacion_financiera": id_operacion_financiera, "es_borrado": false })
-            .sort({ "numero_cuota": 1 });
+const obtener_operacion_financiera_detalle = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const operacion_financiera_detalle = await OperacionFinancieraDetalle.findById(
+      id
+    );
+    return res.json({
+      ok: true,
+      operacion_financiera_detalle,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Error inesperado",
+    });
+  }
+};
 
-        res.json({
-            ok: true,
-            lista
-        })
-    } catch (error) {
+const actualizar_operacion_financiera_detalle = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const operacion_financiera_detalle = await OperacionFinancieraDetalle.findById(
+      id
+    );
+    operacion_financiera_detalle.ingresos.monto_amortizacion_capital =
+      req.body.cuota_pago_capital;
+    operacion_financiera_detalle.ingresos.monto_interes =
+      req.body.cuota_pago_interes;
+    operacion_financiera_detalle.ahorros.monto_ahorro_programado =
+      req.body.cuota_ahorro_programado;
 
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado.'
-        });
-    }
-}
+    await operacion_financiera_detalle.save();
+    return res.json({
+      ok: true,
+      msg: "cuota actualizada",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Error inesperado",
+    });
+  }
+};
+
+const operacion_financiera_detalle_baja = async (req, res) => {
+  const id = req.params.id;
+  const operacion_financiera_detalle = await OperacionFinancieraDetalle.findById(
+    id
+  );
+
+  operacion_financiera_detalle.es_vigente = false;
+  operacion_financiera_detalle.save();
+  return res.json({
+      ok:false,
+      msg:'Cuota dada de baja'
+  })
+};
 
 module.exports = {
-
-    listar_operaciones_financieras_detalle
-}
+  listar_operaciones_financieras_detalle,
+  obtener_operacion_financiera_detalle,
+  actualizar_operacion_financiera_detalle,
+  operacion_financiera_detalle_baja
+};
--- controllers/core/registro/personas.controller.js
+++ controllers/core/registro/personas.controller.js
@@ -1,265 +1,295 @@
-const { response } = require('express');
-const Persona = require('../../../models/core/registro/persona.model');
-const dayjs = require('dayjs');
+const { response } = require("express");
+const Persona = require("../../../models/core/registro/persona.model");
+const Ubigeo = require("../../../models/core/ubigeo.model");
+const dayjs = require("dayjs");
 
-const listar = async(req, res) => {
+const listar = async (req, res) => {
+  const modelo = await Persona.find({ es_borrado: false });
 
-    const modelo = await Persona.find({ "es_borrado": false });
+  res.json({
+    ok: true,
+    modelo,
+  });
+};
 
-    res.json({
-        ok: true,
-        modelo
-    })
-}
+const actualizar = async (req, res = response) => {
+  const id = req.params.id;
+  const uid = req.uid;
 
+  const { comentario } = req.body;
+  console.log(comentario);
 
-const actualizar = async(req, res = response) => {
+  try {
+    const persona = await Persona.findById(id);
 
-    const id = req.params.id;
-    const uid = req.uid;
+    if (!persona) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Persona no encontrada",
+      });
+    }
 
-    const { comentario } = req.body;
-    console.log(comentario);
+    const modelo = await Persona.findById(id);
+    const now = dayjs();
 
-    try {
+    (modelo.nombre = req.body.nombre),
+      (modelo.apellido_paterno = req.body.apellido_paterno),
+      (modelo.apellido_materno = req.body.apellido_materno),
+      (modelo.documento_identidad = req.body.documento_identidad),
+      (modelo.fecha_nacimiento = req.body.fecha_nacimiento),
+      (modelo.es_masculino = req.body.es_masculino),
+      (modelo.numero_telefono = req.body.numero_telefono),
+      (modelo.numero_celular = req.body.numero_celular),
+      (modelo.correo_electronico = req.body.correo_electronico),
+      (modelo.domicilio = req.body.domicilio),
+      (modelo.referencia_domicilio = req.body.referencia_domicilio),
+      (modelo.avatar = req.body.avatar),
+      modelo.comentario.push({
+        tipo: "Editado",
+        idUsuario: req.header("id_usuario_sesion"),
+        usuario: req.header("usuario_sesion"),
+        nombre: req.header("nombre_sesion"),
+        fecha: now.format("DD/MM/YYYY hh:mm:ss a"),
+        comentario,
+      });
 
-        const persona = await Persona.findById(id)
+    console.log(req.header("x-token"));
+    console.log(req.header("id_usuario_sesion"));
+    console.log(req.header("usuario_sesion"));
+    console.log(req.header("nombre_sesion"));
 
-        if (!persona) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'Persona no encontrada'
-            })
-        }
-
-        const modelo = await Persona.findById(id) 
-        const now = dayjs();
-
-        modelo.nombre = req.body.nombre,
-        modelo.apellido_paterno = req.body.apellido_paterno,                
-        modelo.apellido_materno = req.body.apellido_materno,
-        modelo.documento_identidad = req.body.documento_identidad,
-        modelo.fecha_nacimiento = req.body.fecha_nacimiento,
-        modelo.es_masculino = req.body.es_masculino,
-        modelo.numero_telefono = req.body.numero_telefono,
-        modelo.numero_celular = req.body.numero_celular,
-        modelo.correo_electronico = req.body.correo_electronico,
-        modelo.domicilio = req.body.domicilio,
-        modelo.referencia_domicilio = req.body.referencia_domicilio,
-        modelo.avatar = req.body.avatar,
-        modelo.comentario.push({
-            tipo: 'Editado',
-            idUsuario: req.header('id_usuario_sesion'),
-            usuario: req.header('usuario_sesion'),
-            nombre: req.header('nombre_sesion'),
-            fecha: now.format('DD/MM/YYYY hh:mm:ss a'),
-            comentario
-        });
-
-        console.log(req.header('x-token'));
-        console.log(req.header('id_usuario_sesion'));
-        console.log(req.header('usuario_sesion'));
-        console.log(req.header('nombre_sesion'));
-
-        await modelo.save(); 
-        /* const cambiosPersona = {
+    await modelo.save();
+    /* const cambiosPersona = {
             ...req.body,
             usuario: uid
         }
         const personaActualizada = await Persona.findByIdAndUpdate(id, cambiosPersona, { new: true }) */
-        res.json({
-            ok: true,
-            msg: 'Actualizar socio'
-             
-        })
-    } catch (error) {
+    res.json({
+      ok: true,
+      msg: "Actualizar socio",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el Adm",
+    });
+  }
+};
 
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Hable con el Adm'
-        })
+const crear = async (req, res = response) => {
+  const { documento_identidad, comentario } = req.body;
+
+  try {
+    const existe_registro = await Persona.findOne({ documento_identidad });
+
+    if (existe_registro) {
+      return res.status(400).json({
+        ok: false,
+        msg: "La persona ya esta registrada.",
+      });
     }
-}
 
+    const modelo = new Persona(req.body);
+    const now = dayjs();
 
-const crear = async(req, res = response) => {
+    modelo.comentario = [
+      {
+        tipo: "Nuevo",
+        usuario: req.header("id_usuario_sesion"),
+        usuario: req.header("usuario_sesion"),
+        nombre: req.header("nombre_sesion"),
+        fecha: now.format("DD/MM/YYYY hh:mm:ss a"),
+        comentario,
+      },
+    ];
+    await modelo.save();
 
-    const { documento_identidad, comentario } = req.body;
+    res.json({
+      ok: true,
+      modelo,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado.",
+    });
+  }
+};
 
-    try {
+const buscar_por_documento_identidad = async (req, res) => {
+  try {
+    const documento_identidad = req.params.documento_identidad;
+    const persona = await Persona.findOne({
+      documento_identidad: documento_identidad,
+      es_borrado: false,
+    });
 
-        const existe_registro = await Persona.findOne({ documento_identidad });
+    res.json({
+      ok: true,
+      persona,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado.",
+    });
+  }
+};
 
-        if (existe_registro) {
+const buscar_id = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const persona = await Persona.findById(id);
 
-            return res.status(400).json({
-                ok: false,
-                msg: 'La persona ya esta registrada.'
-            });
+    res.json({
+      ok: true,
+      persona,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+};
+
+const buscar_por_nombre = async (req, res) => {
+  const desde = Number(req.query.desde) || 0;
+  try {
+    let termino = req.params.termino;
+    let regex = new RegExp(termino, "i");
+
+    const persona = await Persona.find(
+      { nombre: regex },
+      "documento_identidad domicilio nombre apellido_paterno apellido_materno"
+    )
+      .limit(15)
+      .skip(desde)
+      .sort({ nombre: 1 });
+    const total = await Persona.find({ nombre: regex }).countDocuments();
+    res.json({
+      ok: true,
+      persona,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+};
+
+const buscar_por_apellido = async (req, res) => {
+  const desde = Number(req.query.desde) || 0;
+  try {
+    let termino = req.params.termino;
+    let regex = new RegExp(termino, "i");
+
+    const persona = await Persona.find(
+      { apellido_paterno: regex },
+      "documento_identidad domicilio nombre apellido_paterno apellido_materno"
+    )
+      .limit(15)
+      .skip(desde)
+      .sort({ nombre: 1 });
+    const total = await Persona.find({
+      apellido_paterno: regex,
+    }).countDocuments();
+
+    res.json({
+      ok: true,
+      persona,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+};
+
+const buscar_por_apellido_mat = async (req, res) => {
+  const desde = Number(req.query.desde) || 0;
+  try {
+    let termino = req.params.termino;
+    let regex = new RegExp(termino, "i");
+
+    const persona = await Persona.find(
+      { apellido_materno: regex },
+      "documento_identidad domicilio nombre apellido_paterno apellido_materno"
+    )
+      .limit(15)
+      .skip(desde)
+      .sort({ nombre: 1 });
+    const total = await Persona.find({
+      apellido_materno: regex,
+    }).countDocuments();
+    res.json({
+      ok: true,
+      persona,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: error.message,
+    });
+  }
+};
+
+const datos_persona_reporte = async (req, res) => {
+  const id = req.params.id;
+  const persona = await Persona.findById(
+    id,
+    "apellido_paterno apellido_materno nombre documento_identidad ubigeo domicilio numero_celular"
+  );
+  u = {};
+  console.log(persona.ubigeo.provincia)
+  const ubigeo = await Ubigeo.findById(persona.ubigeo.departamento);
+  u["departamento"] = ubigeo.departamento;
+  ubigeo.provincias.forEach((p) => {
+    if (String(p._id) === String(persona.ubigeo.provincia)) {
+      u["provincia"] = p.provincia;
+      p.distritos.forEach((d) => {
+        if (String(d._id) === String(persona.ubigeo.distrito)) {
+          u["distrito"] = d.distrito;
         }
-
-        const modelo = new Persona(req.body);
-        const now = dayjs();
-
-        modelo.comentario = [{
-            tipo: 'Nuevo',
-            usuario: req.header('id_usuario_sesion'),
-            usuario: req.header('usuario_sesion'),
-            nombre: req.header('nombre_sesion'),
-            fecha: now.format('DD/MM/YYYY hh:mm:ss a'),
-            comentario
-        }];
-        await modelo.save();
-
-        res.json({
-            ok: true,
-            modelo
-        })
-
-    } catch (error) {
-
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado.'
-        });
+      });
     }
-}
+  });
 
-const buscar_por_documento_identidad = async(req, res) => {
-
-    try {
-
-        const documento_identidad = req.params.documento_identidad;
-        const persona = await Persona.findOne({ "documento_identidad": documento_identidad, "es_borrado": false });
-
-
-        res.json({
-            ok: true,
-            persona
-        })
-    } catch (error) {
-
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado.'
-        });
-    }
-}
-
-const buscar_id = async(req, res) => {
-
-    try {
-
-        const id = req.params.id;
-        const persona = await Persona.findById(id);
-
-        res.json({
-            ok: true,
-            persona
-        })
-    } catch (error) {
-
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: error.message
-        });
-    }
-}
-
-const buscar_por_nombre = async(req, res) => {
-    const desde = Number(req.query.desde) || 0;
-    try {
-        let termino = req.params.termino;
-        let regex = new RegExp(termino, 'i')
-
-        const persona = await Persona.find({nombre: regex}, 
-            "documento_identidad domicilio nombre apellido_paterno apellido_materno")
-            .limit(15)
-            .skip(desde)
-            .sort({"nombre": 1});
-        const total = await Persona.find({nombre: regex}).countDocuments()
-        res.json({
-            ok: true,
-            persona,
-            total
-        })
-        
-    } catch (error) {
-         console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: error.message
-        });
-    }
-}
-
-const buscar_por_apellido = async(req, res) => {
-    const desde = Number(req.query.desde) || 0;
-    try {
-        let termino = req.params.termino;
-        let regex = new RegExp(termino, 'i')
-
-        const persona = await Persona.find({apellido_paterno: regex}, 
-            "documento_identidad domicilio nombre apellido_paterno apellido_materno")
-            .limit(15)
-            .skip(desde)
-            .sort({"nombre": 1});;
-        const total = await Persona.find({apellido_paterno: regex}).countDocuments()
-        
-        res.json({
-            ok: true,
-            persona,
-            total
-        })
-        
-    } catch (error) {
-         console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: error.message
-        });
-    }
-}
-
-const buscar_por_apellido_mat = async(req, res) => {
-    const desde = Number(req.query.desde) || 0;
-    try {
-        let termino = req.params.termino;
-        let regex = new RegExp(termino, 'i')
-
-        const persona = await Persona.find({apellido_materno: regex}, 
-            "documento_identidad domicilio nombre apellido_paterno apellido_materno")
-            .limit(15)
-            .skip(desde)
-            .sort({"nombre": 1});;
-        const total = await Persona.find({apellido_materno: regex}).countDocuments()  
-        res.json({
-            ok: true,
-            persona,
-            total
-        })
-        
-    } catch (error) {
-         console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: error.message
-        });
-    }
-}
-
+  const model = {
+      numero_celular:persona.numero_celular,
+      nombre: persona.nombre,
+      documento_identidad: persona.documento_identidad,
+      domicilio: persona.domicilio,
+      apellido_paterno: persona.apellido_paterno,
+      apellido_materno: persona.apellido_materno,
+      ubigeo: u
+  }
+  return res.json({
+    ok: true,
+    persona: model,
+  });
+};
 
 module.exports = {
-    listar,
-    crear,
-    actualizar,
-    buscar_por_documento_identidad,
-    buscar_id,
-    buscar_por_nombre,
-    buscar_por_apellido,
-    buscar_por_apellido_mat
-}
+  listar,
+  crear,
+  actualizar,
+  buscar_por_documento_identidad,
+  buscar_id,
+  buscar_por_nombre,
+  buscar_por_apellido,
+  buscar_por_apellido_mat,
+  datos_persona_reporte,
+};
